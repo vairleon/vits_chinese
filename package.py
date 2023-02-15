200da52a76c256d080ccdd299b011d8a70593f5c
vairleon <vairleon@outlook.com>
2023-02-16 02:36:22 +0800

    fix : corretly speakout

--- package.py
+++ package.py
@@ -12,11 +12,12 @@ import utils
 from models import SynthesizerTrn
 from text.symbols import symbols
 from text import text_to_sequence
-
 from scipy.io import wavfile
+
 import pyaudio
 import wave
-
+import soundfile
+# from ao import AudioDevice
 
 ###########################
 #### Global Configs
@@ -44,8 +45,7 @@ class TTS_Model:
         
         # player
         self.audio = pyaudio.PyAudio()
-        
-        
+    
     def __init_chinese(self):
         hps = utils.get_hparams_from_file(os.path.join(dirpath, "configs/woman_csmsc.json"))
 
@@ -73,6 +73,24 @@ class TTS_Model:
         _ = utils.load_checkpoint(os.path.join(dirpath, "models/pretrained_ljs.pth"), net_g, None) 
         
         return net_g, hps
+    
+        
+    # def __readRawframes(self, nframes):
+    #     if self._data_seek_needed:
+    #         self._data_chunk.seek(0, 0)
+    #         pos = self._soundpos * self._framesize
+    #         if pos:
+    #             self._data_chunk.seek(pos, 0)
+    #         self._data_seek_needed = 0
+    #     if nframes == 0:
+    #         return b''
+    #     data = self._data_chunk.read(nframes * self._framesize)
+    #     if SAMP != 1 and sys.byteorder == 'big':
+    #         data = audioop.byteswap(data, self._sampwidth)
+    #     if self._convert and data:
+    #         data = self._convert(data)
+    #     self._soundpos = self._soundpos + len(data) // (self._nchannels * self._sampwidth)
+    #     return data
             
     def get_text(self, text, hps):
         text_norm = text_to_sequence(text, hps.data.text_cleaners)
@@ -83,7 +101,7 @@ class TTS_Model:
 
     def preprocess(self, text : str):
         if self.chinese_mode:
-            return text.strip().replace(',', '，').replace('.', '。')
+            return text.strip().replace(',', '，').replace('.', '。').replace('?','？').replace("!","！")
         else:
             return text.strip()
     
@@ -95,25 +113,36 @@ class TTS_Model:
             x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
             audio = self.net_g.infer(x_tst, x_tst_lengths, noise_scale=self.noise_scale, noise_scale_w=self.noise_scale_w, length_scale=1)[0][0,0].data.cpu().float().numpy()
         return audio
+
     ##
     # TODO : FIX This speech function 
     def text2speechAndPlay(self, text):
-        audio = self.text2speech(text)
-         # ipd.display(ipd.Audio(audio, rate=self.sampling_rate, normalize=False)) #self.hps.data.sampling_rate
-        print(len(audio))
-        stream=self.audio.open(
-            format=pyaudio.paFloat32, channels=CHANNELS, rate=self.sampling_rate, output=True)
-        # data = wf.readframes(CHUNK)
-        stream.write(audio)
-        stream.close()
+        raw_audio = self.text2speech(text)
+        # ipd.display(ipd.Audio(audio, rate=self.sampling_rate, normalize=False)) #self.hps.data.sampling_rate
+        # print(len(audio))        
+        # os.makedirs(os.path.dirname(TEMP_WAVE_FILE), exist_ok=True)
+        # wavfile.write(TEMP_WAVE_FILE, self.sampling_rate, audio)
+        soundfile.write(TEMP_WAVE_FILE, raw_audio, self.sampling_rate, subtype='PCM_16')
+        #
+
+        # play
+        with wave.open(TEMP_WAVE_FILE, 'rb') as wf:
+            p = pyaudio.PyAudio()
+            stream=p.open(
+                format=pyaudio.paInt16, channels=CHANNELS, rate=self.sampling_rate, output=True)
+        
+            while len(data := wf.readframes(CHUNK)):  
+                stream.write(data)
+                
+            stream.close()
+            p.terminate()
 
         
     def text2wav(self, text, filename=TEMP_WAVE_FILE):
         audio = self.text2speech(text)
         os.makedirs(os.path.dirname(TEMP_WAVE_FILE), exist_ok=True)
-        wavfile.write(filename, self.sampling_rate, audio)
+        wavfile.write(filename, self.sampling_rate, audio.astype(np.int16))
         
-            
 
 if __name__ == "__main__":
     model = TTS_Model()
